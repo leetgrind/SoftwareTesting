dccefa0174864629b36ec4570315cb579bfd1e0b
leetgrind <19754841+aceinthedeck@users.noreply.github.com>
2025-01-16 19:38:57 -0500

    init

---
+++ Debugging2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Debugging2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            int a = 10;
+            int b = 2;
+
+            float answer = divide(a, b);
+
+            Console.WriteLine(answer);
+ 
+        }
+
+        public static float divide(int a, int b)
+        {
+            Console.WriteLine("Dividing " + a + " by " + b);
+
+            return a/b;
+        }
+    }
+}
